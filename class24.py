2a04cb3ff322ff487ace1a27ae79b7835417e5e9
kenny lin <kenny320555@gmail.com>
2021-10-08 01:16:40 +0800

    123

---
+++ class24.py
@@ -0,0 +1,16 @@
+product = []
+while True:
+    name = input('請輸入商品名稱')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格')
+    price = int(price)
+    product.append([name, price])
+print(product)
+
+for p in product:
+    print(p[0], '的價格是', p[1])
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in product:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
